a692a94908c0b7cc32620cab2a383650454e9601
alzaslon <alzaslon@microsoft.com>
2017-09-02 12:24:29 -0700

    added anchors support. cleanup.

--- src/utils.ts
+++ src/utils.ts
@@ -1,51 +1,62 @@
-import * as React from 'react';
-import { Set, Seq, Collection, List, Map } from 'immutable';
+import * as React from "react";
 
 export class Utils {
     public static createElement(tagName) {
-        return props => {
-            return Utils.createReactElement(tagName, props);
+        return properties => {
+            return Utils.createReactElement(tagName, properties);
         }
     }
 
-    public static createReactElement(tagName, props) {
-        let data = props.mark ? props.mark.data : props.node.data;
-        let intentions = data.get("intentions");
-        let categories = data.get("categories");
+    public static createReactElement(tagName: string, properties: any): JSX.Element {
+        const data = properties.mark ? properties.mark.data : properties.node.data;
+        const categories = data.get("categories");
 
-        if (!intentions && !categories) {
-            return React.createElement(tagName, props.attributes, props.children)
+        if (!categories) {
+            return React.createElement(tagName, properties.attributes, properties.children);
         }
 
-        if (!intentions) {
-            intentions = List(Object.keys(categories).map(k => categories[k]))
-        } else if (categories) {
-            intentions = List(Set(intentions).union(Object.keys(categories).map(k => categories[k])))
-        } else {
-            intentions = List(intentions)
+        const attributes = properties.attributes || {};
+
+        const classNameCategoryKeys = Object.keys(categories).filter(x => x !== "anchorKey");
+
+        if (classNameCategoryKeys.length > 0) {
+            const className = classNameCategoryKeys
+                .map(category => categories[category])
+                .map(intentionKey => {
+                    const intentionFunc = Utils.Configuration.IntentionsMap[intentionKey];
+
+                    if (!intentionFunc) {
+                        console.warn(`Could not find intention with key ${intentionKey}`);
+                        return "";
+                    }
+
+                    return intentionFunc();
+                })
+                .join(" ");
+
+            Object.assign(attributes, { className: className });
         }
 
-        let className = intentions
-            .update((collection) => {
-                return collection.reduce((cn, fn) => cn + " " + Utils.Configuration.IntentionsMap[fn](), "")
-            })
-            .trim();
+        const anchorCategoryKey = Object.keys(categories).find(x => x === "anchorKey");
 
-        let attr = props.attributes || {}
-        Object.assign(attr, { className: className })
+        if (anchorCategoryKey) {
+            const id = categories[anchorCategoryKey];
 
-        return props.children ? React.createElement(tagName, attr, props.children) : React.createElement(tagName, attr);
+            Object.assign(attributes, { id: id });
+        }
+
+        return properties.children ? React.createElement(tagName, attributes, properties.children) : React.createElement(tagName, attributes);
     }
 
     public static createLinkElement() {
-        return props => {
-            const { data } = props.node;
-            const href = data.get('href');
-            const target = data.get('target');
+        return properties => {
+            const { data } = properties.node;
+            const href = data.get("href");
+            const target = data.get("target");
 
-            Object.assign(props.attributes, { href: href, target: target });
+            Object.assign(properties.attributes, { href: href, target: target });
 
-            return Utils.createReactElement("a", props);
+            return Utils.createReactElement("a", properties);
         }
     }
 
@@ -53,26 +64,26 @@ export class Utils {
         IntentionsMap: {},
         Schema: {
             nodes: {
-                'block-quote': Utils.createElement('blockquote'),
-                'bulleted-list': Utils.createElement('ul'),
-                'heading-one': Utils.createElement('h1'),
-                'heading-two': Utils.createElement('h2'),
-                'heading-three': Utils.createElement('h3'),
-                'heading-four': Utils.createElement('h4'),
-                'heading-five': Utils.createElement('h5'),
-                'heading-six': Utils.createElement('h6'),
-                'list-item': Utils.createElement('li'),
-                'numbered-list': Utils.createElement('ol'),
-                'link': Utils.createLinkElement(),
-                'code': Utils.createElement('pre'),
-                'paragraph': Utils.createElement("p"),
-                'custom': Utils.createElement("div")
+                "block-quote": Utils.createElement("blockquote"),
+                "bulleted-list": Utils.createElement("ul"),
+                "heading-one": Utils.createElement("h1"),
+                "heading-two": Utils.createElement("h2"),
+                "heading-three": Utils.createElement("h3"),
+                "heading-four": Utils.createElement("h4"),
+                "heading-five": Utils.createElement("h5"),
+                "heading-six": Utils.createElement("h6"),
+                "list-item": Utils.createElement("li"),
+                "numbered-list": Utils.createElement("ol"),
+                "link": Utils.createLinkElement(),
+                "code": Utils.createElement("pre"),
+                "paragraph": Utils.createElement("p"),
+                "custom": Utils.createElement("div")
             },
             marks: {
-                bold: Utils.createElement('b'),
-                italic: Utils.createElement('i'),
-                underlined: Utils.createElement('u'),
-                custom: Utils.createElement('span')
+                bold: Utils.createElement("b"),
+                italic: Utils.createElement("i"),
+                underlined: Utils.createElement("u"),
+                custom: Utils.createElement("span")
             }
         }
     }
